c4dd29efac79e7c06640b610f03fbaaa0975474f
KatyaZanina <katyazanina95@gmail.com>
2021-11-04 15:47:00 +0300

    add preloader and mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import './components/social';
 import {ieFix} from './vendor/ie-fix';
 import {vhFix} from './vendor/vh-fix';
 import {actualYear} from './modules/actualYear';
+import {toRunPreloader} from './modules/preloader';
 import header from './components/header';
 import lazyLoading from './modules/lazyLoading';
 import scrollToAnchor from './modules/scrollToAnchor';
@@ -15,3 +16,4 @@ scrollToAnchor.init();
 
 header.init();
 lazyLoading.init();
+toRunPreloader();
